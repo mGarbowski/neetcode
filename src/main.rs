2a2d590e0f440b2e0a38806bab4e6b84838fe9f2
mgarbowski <mikolaj.garbowski@gmail.com>
2024-02-06 18:36:30 +0100

    solved top k frequent using bucket sort

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn count_chars(s: String) -> HashMap<char, i32> {
     char_counts
 }
 
-fn count_elements<T>(elements: Vec<T>) -> HashMap<T, i32>
+fn count_elements<T>(elements: Vec<T>) -> HashMap<T, usize>
     where T: Eq + Hash
 {
     let mut counts = HashMap::new();
@@ -77,14 +77,26 @@ fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
 
 /// https://leetcode.com/problems/top-k-frequent-elements/
 fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let n_counts = nums.len() + 1;
     let counts = count_elements(nums);
-    let mut entries: Vec<(i32, i32)> = counts.into_iter().collect();
-    entries.sort_by_key(|en| -en.1);
-    entries.iter()
-        .take(k as usize)
-        .cloned()
-        .map(|(num, _)| num)
-        .collect()
+    let mut buckets: Vec<Vec<i32>> = vec![vec![]; n_counts];
+
+    for (num, count) in counts {
+        buckets.get_mut(count).unwrap().push(num)
+    }
+
+    let mut result = Vec::new();
+    let mut idx = n_counts-1;
+    while result.len() < k as usize {
+        let bucket = buckets.get_mut(idx).unwrap();
+        if bucket.len() > 0 {
+            result.append(bucket);
+        }
+
+        idx -= 1;
+    }
+
+    result
 }
 
 #[cfg(test)]
