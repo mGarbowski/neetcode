9058d7df1a483327a571849732222bc6da7edd64
mgarbowski <mikolaj.garbowski@gmail.com>
2024-02-17 16:03:02 +0100

    two pointer solution to trapping rain water

--- src/two_pointers/trapping_rain_water.rs
+++ src/two_pointers/trapping_rain_water.rs
@@ -3,27 +3,34 @@
 use std::cmp::{max, min};
 
 pub fn trap(height: Vec<i32>) -> i32 {
-    let n = height.len();
-    if n <= 2 {
-        return 0;
-    }
-
-    let mut max_height_to_left = vec![0; n];
-    for i in 1..n {
-        max_height_to_left[i] = max(max_height_to_left[i - 1], height[i - 1]);
-    }
-
-    let mut max_height_to_right = vec![0; n];
-    for i in (0..=n - 2).rev() {
-        max_height_to_right[i] = max(max_height_to_right[i + 1], height[i + 1]);
-    }
-
     let mut total_area = 0;
-    for i in 0..n {
-        let bounding_height = min(max_height_to_left[i], max_height_to_right[i]);
-        let water_area = bounding_height - height[i];
-        total_area += max(water_area, 0);
+    let mut left = 0;
+    let mut right = height.len() - 1;
+    let mut max_left_height = height[left];
+    let mut max_right_height = height[right];
+    let mut current_level;
+
+    while left < right {
+        let advancing_left = max_left_height <= max_right_height;
+        if advancing_left {
+            left += 1;
+            current_level = height[left];
+        } else {
+            right -= 1;
+            current_level = height[right];
+        }
+
+        let bounding_height = min(max_left_height, max_right_height);
+        let bar_height = max(bounding_height - current_level, 0);
+        total_area += bar_height;
+
+        if advancing_left {
+            max_left_height = max(current_level, max_left_height);
+        } else {
+            max_right_height = max(current_level, max_right_height);
+        }
     }
+
     total_area
 }
 
