5817f8866268bb8406ad2fdd145aa9062f70bb10
mgarbowski <mikolaj.garbowski@gmail.com>
2024-02-17 16:06:55 +0100

    refactor trapping rain water

--- src/two_pointers/trapping_rain_water.rs
+++ src/two_pointers/trapping_rain_water.rs
@@ -1,6 +1,6 @@
 //! https://leetcode.com/problems/trapping-rain-water/
 
-use std::cmp::{max, min};
+use std::cmp::max;
 
 pub fn trap(height: Vec<i32>) -> i32 {
     let mut total_area = 0;
@@ -8,26 +8,16 @@ pub fn trap(height: Vec<i32>) -> i32 {
     let mut right = height.len() - 1;
     let mut max_left_height = height[left];
     let mut max_right_height = height[right];
-    let mut current_level;
 
     while left < right {
-        let advancing_left = max_left_height <= max_right_height;
-        if advancing_left {
+        if max_left_height <= max_right_height {
             left += 1;
-            current_level = height[left];
+            max_left_height = max(height[left], max_left_height);
+            total_area += max_left_height - height[left];
         } else {
             right -= 1;
-            current_level = height[right];
-        }
-
-        let bounding_height = min(max_left_height, max_right_height);
-        let bar_height = max(bounding_height - current_level, 0);
-        total_area += bar_height;
-
-        if advancing_left {
-            max_left_height = max(current_level, max_left_height);
-        } else {
-            max_right_height = max(current_level, max_right_height);
+            max_right_height = max(height[right], max_right_height);
+            total_area += max_right_height - height[right];
         }
     }
 
